b599b3a81e6d2e81a12d81dbe0e8f04c82b35811
abdurazzaaqabdallaa-svg <abdurazzaaqabdallaa@gmail.com>
2026-05-01 19:24:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,19 @@
-self.addEventListener('fetch', function(event) {
-  // Ammaaf waan hunda interneeta irraa akka fidu qofa godha
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'qoricha-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
