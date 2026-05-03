ab27b2664ce90d426d1e7e632abb4a93f63a7ed5
abdurazzaaqabdallaa-svg <abdurazzaaqabdallaa@gmail.com>
2026-05-03 03:22:39 +0300

    Update service worker cache name and structure

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'qoricha-v1';
+const CACHE_NAME = 'qoricha-aadaa-v1';
 const assets = [
   './',
   './index.html',
@@ -6,37 +6,33 @@ const assets = [
   './icon-512.png'
 ];
 
+// 1. Install - Fayiilota kuusaa (cache) keessa kaa'uuf
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
-const cacheName = 'qoricha-cache-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Fayiilonni cache keessa galaniiru');
+      return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(rec => {
-      return rec || fetch(evt.request);
+// 2. Activate - Kuusaa durii (old cache) qulqulleessuuf
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// 3. Fetch - Offline yeroo ta'u kuusaa irraa akka fiduuf
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
